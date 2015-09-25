07ec33d95a5c05e9080bde8374f4a5f6dd5ba609
Zack <cstony0917@gmail.com>
2015-09-25 11:30:16 +0800

    Create get_branches.php

---
+++ git/get_branches.php
@@ -0,0 +1,33 @@
+function get_branches(){
+	// branches, bs
+	$bs = shell_exec('git branch');
+	$bs = explode(PHP_EOL, $bs);
+
+	// output
+	$o = array();
+
+	foreach ($bs as $k => $b) {
+		if( strlen($b) <= 0 ){
+			unset($bs[$k]);
+		}else{
+			$b = trim($b);
+
+			// contains * string is the current branch
+			if( strpos($b, '*') !== false ){
+				$o['current'] = explode(' ', $b)[1];
+			}
+			// with / string means it's a nested branch
+			else if( strpos($b, '/') !== false){
+				$exploded = explode('/', $b);
+				$key = $exploded[0];
+				$o[$key][] = $exploded[1];
+			}
+		}
+	}
+
+	return $o;
+}
+
+function echo_branches(){
+	echo json_encode( get_branches() );
+}
